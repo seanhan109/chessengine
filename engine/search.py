0a6d4d785ec618a46066822f5e01c520321a533c
seanhan109 <seanhan2025@u.northwestern.edu>
2023-04-17 00:47:07 -0500

    added quiescence search

--- engine/search.py
+++ engine/search.py
@@ -6,41 +6,41 @@ from eval import eval
 
 class FindMove():
     def __init__(self, board, max_depth):
-        self.max_depth = max_depth
+        self.depth = max_depth
         self.board = board
-        self.pv = [None] * max_depth
+        self.next_move = []
     
     def alpha_beta(self, a, b, depth)->int:
         if depth == 0:
-            return eval(self.board)
-        util = float('-inf')
-        # Maybe not right to add here
-        # Might be issues with pv updates
-        if self.pv[depth - 1]:
-            self.board.push(self.pv[depth-1])
-            score = -self.alpha_beta(-b,-a,depth - 1)
-            if score >= b:
-                return score
-            if score > util:
-                util = score
-            if score > a:
-                a = score
-                self.pv[depth - 1] = legal_move
-            self.board.pop()
+            return self.quiescence(a, b)
         legal_moves = self.board.legal_moves
         #sort moves in order of most promising
         for legal_move in legal_moves:
             self.board.push(legal_move)
             score = -self.alpha_beta(-b, -a, depth - 1)
+            self.board.pop()
             if score >= b:
                 return score
-            if score > util:
-                util = score
             if score > a:
                 a = score
-                self.pv[depth - 1] = legal_move
-            self.board.pop()
         return a
     
-    def play_move(self, move)->None:
-        self.board.push(move)
+    def quiescence(self, a, b)->int:
+        stand_pat = eval(self.board)
+        if (stand_pat >= b):
+            return b
+        if (stand_pat > a):
+            a = stand_pat
+        legal_moves = self.board.legal_moves
+        for legal_move in legal_moves:
+            if not self.board.is_capture(legal_move):
+                continue
+            self.board.push(legal_move)
+            score = -self.quiescence(-b, -a)
+            self.board.pop()
+
+            if score >= b:
+                return b
+            if score > a:
+                a = score
+        return a
